6e61915c4882225f29842faddcb438cd67c69c40
Jayson Haulkory <haulkory.jayson@gmail.com>
2022-01-03 14:48:42 +0100

    first commit

--- src/com/bootcamp/cesi/function/Ex1.java
+++ src/com/bootcamp/cesi/function/Ex1.java
@@ -2,6 +2,10 @@ package com.bootcamp.cesi.function;
 
 public class Ex1 {
     public static void main(String[] args) {
+        affichage();
+    }
 
+    public static void affichage(){
+        System.out.println("Je suis une fonction") ;
     }
 }
